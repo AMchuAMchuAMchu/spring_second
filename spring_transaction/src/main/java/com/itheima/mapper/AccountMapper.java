12384edb91ac5a58526daf5ce36482c60fd3e7f5
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-09 18:19:49 +0800

    AM 01st commit

---
+++ spring_transaction/src/main/java/com/itheima/mapper/AccountMapper.java
@@ -0,0 +1,22 @@
+package com.itheima.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_second
+ * BelongsPackage ==> com.itheima.mapper
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-09 18:09:07
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+public interface AccountMapper {
+
+    @Insert("update account set money = money + #{money} where id = 1")
+    void insertAdd(@Param("money") int money);
+
+    @Insert("update account set money = money - #{money} where id = 2")
+    void insertSub(@Param("money") int money);
+
+}
