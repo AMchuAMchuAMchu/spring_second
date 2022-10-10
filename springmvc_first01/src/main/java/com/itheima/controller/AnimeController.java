a1c5f3004e890b49610e937f7a1b779ee81cb887
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-10 11:32:48 +0800

    AM 01st commit

---
+++ springmvc_first01/src/main/java/com/itheima/controller/AnimeController.java
@@ -0,0 +1,18 @@
+package com.itheima.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Description ==> TODO
+ * BelongsProject ==> spring_second
+ * BelongsPackage ==> com.itheima.controller
+ * Version ==> 1.0
+ * CreateTime ==> 2022-10-10 11:32:29
+ * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
+ */
+@Controller
+public class AnimeController {
+
+
+
+}
--- springmvc_first01/src/main/java/com/itheima/springmvcconfig/MVCInit.java
+++ springmvc_first01/src/main/java/com/itheima/springmvcconfig/MVCInit.java
@@ -1,5 +1,10 @@
 package com.itheima.springmvcconfig;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
 /**
  * Description ==> TODO
  * BelongsProject ==> spring_second
@@ -8,5 +13,24 @@ package com.itheima.springmvcconfig;
  * CreateTime ==> 2022-10-10 11:29:46
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
-public class MVCInit {
+@Controller
+public class MVCInit extends AbstractDispatcherServletInitializer {
+
+
+    @Override
+    protected WebApplicationContext createServletApplicationContext() {
+        AnnotationConfigWebApplicationContext acmvc = new AnnotationConfigWebApplicationContext();
+        acmvc.register(SpringMVCConfig.class);
+        return acmvc;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected WebApplicationContext createRootApplicationContext() {
+        return null;
+    }
 }
